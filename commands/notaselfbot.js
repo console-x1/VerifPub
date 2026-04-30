8fe50a451fe4a3eb2a6267168b1a9945228afe0e
console-x1 <x1.fonda@gmail.com>
2026-04-29 23:08:46 -0300

    J'ai dev avec mon cul, je connais mm pas cet ID

--- commands/notaselfbot.js
+++ commands/notaselfbot.js
@@ -11,7 +11,6 @@ module.exports = {
 
     async execute(client, message, args) {
         let content;
-        if (client.config.id !== '1405597638199480434') content = `<:staff:1454546318738329926> **Accès interdit ! Commande réservé aux développeurs !** <:staff:1454546318738329926>`
 
         if (!content) {
             if (!args || ((!args[0] && !args[1]) || (args[0] !== 'remove' && args[0] !== 'add'))) content = `<:staff:1454546318738329926> Utilisation correcte : ${client.config.prefix}not-a-self <add|remove> <userId> <:staff:1454546318738329926>`
@@ -37,7 +36,6 @@ module.exports = {
         let content;
         const user = interaction.options.getUser('user').id
         const action = interaction.options.getString('action')
-        if (client.config.id !== '1405597638199480434') content = `<:staff:1454546318738329926> **Accès interdit ! Commande réservé aux développeurs !** <:staff:1454546318738329926>`
 
         if (!content) {
             if (action !== 'add') self.removeNotSelfId(user)
